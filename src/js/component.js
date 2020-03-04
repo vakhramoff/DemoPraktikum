7d25d0b545f294f467ff91778722f19c39030710
Sergey Vakhramov <vakhramoff@xakep.ru>
2020-03-04 15:40:33 +0500

    Deleted old files.

--- src/js/component.js
+++
@@ -1,9 +0,0 @@
-class Component {
-    constructor() {
-        if (new.target === Component) {
-            throw new Error(`Can't instantiate Component`);
-        }
-    }
-}
-
-export default Component;
